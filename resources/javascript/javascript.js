29fe6afb4906d5ceddf1f80439336ab062ae2778
Ber2020 <berraksu2002@yahoo.com>
2021-01-06 22:54:56 +0000

    Typewriter; coding started

--- resources/javascript/javascript.js
+++ resources/javascript/javascript.js
@@ -37,4 +37,19 @@ startButton.addEventListener("click", startCarousel);
 stopButton.addEventListener("click", stopCarousel);
 
 
+// Typewriter
+
+var i = 0, text;
+text = "welcome to my website"
+
+function typing() {
+    if (i < text.length) {
+        document.getElementById("text").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, 50);
+    }
+}
+typing();
+    
+
 
