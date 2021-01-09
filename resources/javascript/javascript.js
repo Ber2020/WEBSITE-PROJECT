40e0e463fa58d5a06310f8f8f22a9cd391aacf46
Ber2020 <berraksu2002@yahoo.com>
2021-01-09 19:54:06 +0000

    .page-footer p selector; padding added css file is formatted with prettier

--- resources/javascript/javascript.js
+++ resources/javascript/javascript.js
@@ -5,40 +5,39 @@ let currentImage = 0;
 let isPlaying = true;
 
 function nextImage() {
-    carouselImages[currentImage].classList.remove("visible");
-    carouselImages[currentImage].classList.add("hidden");
-    currentImage++;
-    checkLimit();
-    carouselImages[currentImage].classList.add("visible");
-    carouselImages[currentImage].classList.remove("hidden");
+  carouselImages[currentImage].classList.remove("visible");
+  carouselImages[currentImage].classList.add("hidden");
+  currentImage++;
+  checkLimit();
+  carouselImages[currentImage].classList.add("visible");
+  carouselImages[currentImage].classList.remove("hidden");
 }
 
 const checkLimit = () => {
-    if (currentImage > carouselImages.length - 1) {
-        currentImage = 0;
-    }
+  if (currentImage > carouselImages.length - 1) {
+    currentImage = 0;
+  }
 };
 
 let carouselInterval = setInterval(nextImage, 3000);
 
 const startCarousel = () => {
-    if (!isPlaying) {
-        isPlaying = true;
-        carouselInterval = setInterval(nextImage, 3000);
-    }
+  if (!isPlaying) {
+    isPlaying = true;
+    carouselInterval = setInterval(nextImage, 3000);
+  }
 };
 
 function stopCarousel() {
-    isPlaying = false;
-    clearInterval(carouselInterval);
-};
+  isPlaying = false;
+  clearInterval(carouselInterval);
+}
 
 startButton.addEventListener("click", startCarousel);
 stopButton.addEventListener("click", stopCarousel);
 
+//  Typewriter
 
-// Typewriter
-  
 const texts = ["welcome to my website"];
 let count = 0;
 let index = 0;
@@ -46,17 +45,16 @@ let currentText = "";
 let letter = "";
 
 (function type() {
-    if (count === texts.length) {
-        count = 0;
-    }
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index);
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
 
-    document.querySelector(".typing").textContent = letter;
-    if (letter.length === currentText.length) {
-        count++;
-        index = 0;
-    }
-    setTimeout(type, 400);
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 400);
 })();
-
